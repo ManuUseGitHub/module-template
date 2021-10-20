2832c5b1c175e497db51797514b0263226a6b6cd
Jean Luc Emmanuel VERHANNEMAN <14911881+ManuUseGitHub@users.noreply.github.com>
2021-10-20 03:26:30 +0200

    Update interfaces.ts

--- dev/interfaces.ts
+++ dev/interfaces.ts
@@ -1,31 +1,3 @@
-export interface IWordStateOptions {
-  sortString : string;
-  minimumLength : number;
-  cbOnNewWord : ( word: string ) => void;
-}
-
-export interface IStateOfWords {
-  order ? : number ,
-  word ? : string ,
-  number ? : number ,
-  length ? : number
-}
-
-export interface IStateOfWordsObject {
-  [ key: string ]: { 
-    order ? : number , 
-    number ? : number , 
-    length ? : number 
-  }
-}
-
-export interface IDictionary {
-  [ key: string ]: number;
-}
-
-export interface ICandidate {
-  dictionary : IDictionary ,
-  length : number ,
-  currentWord : string[] ,
-  options : IWordStateOptions
+export interface IsuperInterface {
+  ...
 }
