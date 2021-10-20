3a8aa979d0da203cf00f5315a8e2dd25d6a36429
Jean Luc Emmanuel VERHANNEMAN <14911881+ManuUseGitHub@users.noreply.github.com>
2021-10-20 03:32:22 +0200

    Update index.js

--- demo/index.js
+++ demo/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const { getWStatsList , getWStatsObj , getWordList } = require( "../atext-wordz/index.js" );
+const { myService } = require( "../module-name/index.js" );
 const fs = require( "fs" );
 
 fs.readFile( "./demo/demo.txt" , "utf8" , ( err , text ) => {
@@ -11,28 +11,16 @@ fs.readFile( "./demo/demo.txt" , "utf8" , ( err , text ) => {
     return;
   }
 
-  const sortString = `
-    by word of a greater than b's
-` ;
-  const minimumLength = 1;
-  const format = "list";
-
-  const prohibitedWords = [ "sexe" , "violence" , "data" , "graphie" , "corps" ];
-  const cbOnNewWord = ( word ) => {
-
-    if ( prohibitedWords.includes( word ) )
-      console.log( "prohibited : " + word );
-  };
-  const options = { sortString , minimumLength , format , cbOnNewWord };
-  const result = getWStatsList( text , options );
-
+  // TODO : manipulate the service
+  const result = myService ( text );
+  const resultText = foo(result);
+  
   const fileCallback = ( err , data ) => { };
 
-  // write a list of all different words in demow.txt
-  fs.writeFile( "./assets/demow.txt" , result.map( w => w.word ).join( "\n" ) , fileCallback );
-
-  // write a the stats of words into demo.json
-  fs.writeFile( "./assets/demo.json" , JSON.stringify( { wordsStats : result } , null , 2 ) , fileCallback );
+ 
+  fs.writeFile( "./assets/demout.txt" , resultText , fileCallback );
+  
+  fs.writeFile( "./assets/demout.json" , JSON.stringify( { result:data } , null , 2 ) , fileCallback );
 
   console.log( result );
 } );
